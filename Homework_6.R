664a77dfb1873c22cb47ff3b24763f9e66a4ba8d
beisenpress <ben@eisenpress.com>
2016-05-19 23:26:50 -0500

    Add raw data. Create R script and add header.

---
+++ Homework_6.R
@@ -0,0 +1,17 @@
+#### 37301 Data Driven Marketing ######
+#### Homework 5 #######
+#### Author: Ben Eisenpress #########
+
+# Load libraries
+library(psych)
+library(lattice)
+library(plyr)
+library(mfx)
+
+# Set working directory
+setwd("/Users/ben/Dropbox/Chicago Booth/37103 Data Driven Marketing/Homework 6/37103-HW6")
+# Load Data
+load("Tuscan-Lifestyles.RData")
+
+########################################################
+################## Question 1 ##########################
